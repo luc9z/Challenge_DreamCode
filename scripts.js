1e6489fda87c8f4097ca6486fd0a87b491bcc3e7
Lucas <lucas123melo2017@gmail.com>
2023-08-09 04:15:24 -0300

    Projeto refeito e feito a solução de bugs.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,37 @@
-const toggleThemeButton = document.querySelector("#toggletheme");
-const body = document.querySelector("body");
+const toggleSwitch = document.querySelector('#toggle');
+const $html = document.querySelector('html')
 
-toggleThemeButton.addEventListener("click", () => {
-  body.classList.toggle("lighttheme");
-
-  if (document.body.classList.contains("lighttheme")) {
-    toggleThemeButton.textContent = "Dark Theme";
+// função para alternar entre os temas
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'light');
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+    $html.classList.add('light-mode');
+    $html.classList.remove('dark-mode');
+    icon.forEach(i => i.classList.remove('hidden'));
   } else {
-    toggleThemeButton.textContent = "Light Theme";
+    document.documentElement.setAttribute('data-theme', 'dark');
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+    $html.classList.add('dark-mode');
+    $html.classList.remove('light-mode');
+    icon.forEach(i => i.classList.add('hidden'));
+  }    
+}
+
+// adiciona o evento de clique ao botão switch
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// detecta se o tema é light por padrão
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'light') {
+    toggleSwitch.checked = true;
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+    icon.forEach(i => i.classList.remove('hidden'));
   }
-});
+}
